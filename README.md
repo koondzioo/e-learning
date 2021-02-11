1e2d2b02e7e2ce29954cff4316ca9b0d7fd982cf
Konrad <koondzoo@gmail.com>
2021-02-11 23:20:58 +0100

    create eureka-server and start working on user-service

---
+++ README.md
@@ -0,0 +1,10 @@
+## RUN CONTAINERS
+    * docker-compose up -d --build
+## LOGS
+    *  docker-compose logs -f
+
+## STOP CONTAINERS
+    *  docker-compose stop
+
+## CLEAN CONTAINERS
+    * docker system prune
---
+++ user-service/src/main/java/com/app/UserController.java
@@ -0,0 +1,18 @@
+package com.app;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+@RequiredArgsConstructor
+public class UserController {
+
+    @GetMapping
+    private String getStatus() {
+        return "OK";
+    }
+}
